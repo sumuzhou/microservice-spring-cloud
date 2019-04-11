64c3658c0f5a0f62f2dfab6e6d3df22936219011
sumuzhou <ggyoung16@gmail.com>
2019-04-11 13:42:28 +0800

    Update ApiGatewayApplication.java

--- api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -2,6 +2,7 @@ package com.example.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
 @SpringBootApplication
